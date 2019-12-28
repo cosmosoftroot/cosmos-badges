f9044921a930e73ac2f172133b96eb5696559c74
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2019-12-28 08:39:57 -0500

    Levantamiento del estado y listado de componentes

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -6,6 +6,24 @@ import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
+  state = { form: {
+    firstName:'',
+    lastName:'',
+    jobTitle:'',
+    email:'',
+    twitter:'',
+
+  } };
+
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
   render() {
     return (
       <div>
@@ -17,15 +35,16 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName="Iván"
-                lastName="Sánchez"
-                jobTitle="Fullstack JavaScript Developer"
-                twitter="cosmosoftroot"
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                jobTitle={this.state.form.jobTitle}
+                twitter={this.state.form.twitter}
+                mail={this.state.form.mail}
                 avatarUrl="https://2.gravatar.com/avatar/b9c674ca7541ea962e68107fbc6cb335?s=400&d=mm"
               />
             </div>
-            <div className='col-6'>
-                <BadgeForm />
+            <div className="col-6">
+              <BadgeForm onChange={this.handleChange} formValues={this.state.form}/>
             </div>
           </div>
         </div>
