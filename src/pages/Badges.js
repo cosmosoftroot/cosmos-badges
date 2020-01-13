cc2a3c99e7a2619dfd59597431f4407640e5bc94
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2020-01-13 07:24:04 -0500

    Ciclo de vida de un componente

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -7,35 +7,70 @@ import BadgesList from "../components/BadgesList";
 import defaultAvatar from "../images/default_avatar.png";
 
 class Badges extends React.Component {
-  state = {
-    data: [
-      {
-        id: "0101",
-        firstName: "Richard",
-        lastName: "Stallman",
-        jobTitle: "Activist and programmer",
-        email: "no.more.microsoft@gmail.com",
-        twitter: "rstallman"
-      },
-      {
-        id: "0104",
-        firstName: "Ivan",
-        lastName: "Sanchez",
-        jobTitle: "Full stack developer",
-        email: "isanchez@comosoft.com",
-        twitter: "cosmosftroot"
-      },
-      {
-        id: "0105",
-        firstName: "Nicola",
-        lastName: "Tesla",
-        jobTitle: "Genius",
-        email: "nicotesla@gmail.com",
-        twitter: "iknowall"
-      }
-    ]
-  };
+  constructor(props) {
+    super(props);
+    console.log("1. constructor()");
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    console.log("3. componentDidMount()");
+
+    this.timeoutId = setTimeout(() => {
+      this.setState({
+        data: [
+          {
+            id: "0101",
+            firstName: "Richard",
+            lastName: "Stallman",
+            jobTitle: "Activist and programmer",
+            email: "no.more.microsoft@gmail.com",
+            twitter: "rstallman"
+          },
+          {
+            id: "0104",
+            firstName: "Ivan",
+            lastName: "Sanchez",
+            jobTitle: "Full stack developer",
+            email: "isanchez@comosoft.com",
+            twitter: "cosmosftroot"
+          },
+          {
+            id: "0105",
+            firstName: "Nicola",
+            lastName: "Tesla",
+            jobTitle: "Genius",
+            email: "nicotesla@gmail.com",
+            twitter: "iknowall"
+          }
+        ]
+      });
+    }, 3000);
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("5. componentDidUpdate()");
+
+    console.log({
+      prevProps: prevProps, prevState: prevState
+    })
+
+    console.log({
+      props: this.props, state: this.state
+    })
+
+  }
+
+  componentWillUnmount(){
+    console.log("6. componentWillUnmount()")
+    clearTimeout(this.timeoutId)
+  }
+
   render() {
+    console.log("2/4. render()");
     return (
       <React.Fragment>
         <div className="Badges">
