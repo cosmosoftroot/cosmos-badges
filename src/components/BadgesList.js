9395a3397c6d0d8e89abb37a890a122e9b5d7891
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2020-01-29 09:10:47 -0500

    Pendiente terminar search filter

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -2,30 +2,33 @@ import React from "react";
 import BadgesListItem from "./BadgeListItem";
 import { Link } from "react-router-dom";
 
-class BadgesList extends React.Component {
-  render() {
-    if (this.props.badges.length === 0) {
-      return (
-        <div>
-          <h5 className="alert alert-secondary text-center">Data not found</h5>
-        </div>
-      );
-    }
+function BadgesList(props) {
+  const badges = props.badges;
 
+  if (badges.length === 0) {
     return (
-      <ul className="list-unstyled">
-        {this.props.badges.map(badge => {
-          return (
-            <li key={badge.id}>
-              <Link className="text-reset text-decoration-none" to={`/badges/${badge.id}`}>
-                <BadgesListItem badge={badge} />
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+      <div>
+        <h5 className="alert alert-secondary text-center">Data not found</h5>
+      </div>
     );
   }
+
+  return (
+    <ul className="list-unstyled">
+      {badges.map(badge => {
+        return (
+          <li key={badge.id}>
+            <Link
+              className="text-reset text-decoration-none"
+              to={`/badges/${badge.id}`}
+            >
+              <BadgesListItem badge={badge} />
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default BadgesList;
--- src/pages/BadgeDetailsContainer.js
+++ src/pages/BadgeDetailsContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PageLoading from "../components/PageLoading";
 import PageError from "../components/PageError";
-import BadgeDetails from './BadgeDetails'
+import BadgeDetails from "./BadgeDetails";
 
 import api from "../api";
 
@@ -9,7 +9,8 @@ class BadgeDetailsContainer extends React.Component {
   state = {
     loading: true,
     error: null,
-    data: undefined
+    data: undefined,
+    modalIsOpen: false
   };
 
   componentDidMount() {
@@ -26,6 +27,25 @@ class BadgeDetailsContainer extends React.Component {
     }
   };
 
+  handleOpenModal = e => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  handleCloseModal = e => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  handleDeleteBadge = async e => {
+    this.setState({ loading: true, error: null });
+    try {
+      await api.badges.remove(this.props.match.params.badgeId);
+      this.setState({ loading: false });
+      this.props.history.push("/badges");
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  };
+
   render() {
     if (this.state.loading) {
       return <PageLoading />;
@@ -35,7 +55,13 @@ class BadgeDetailsContainer extends React.Component {
     }
 
     return (
-      <BadgeDetails badge={this.state.data}/>
+      <BadgeDetails
+        badge={this.state.data}
+        onOpenModal={this.handleOpenModal}
+        onCloseModal={this.handleCloseModal}
+        modalIsOpen={this.state.modalIsOpen}
+        onDeleteBadge={this.handleDeleteBadge}
+      />
     );
   }
 }
